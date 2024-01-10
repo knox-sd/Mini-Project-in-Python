01ad0ab083ccffca0e0646afdf85747a66f43eb7
Sujan Adhikari <sujan.ad6473@gmail.com>
2024-01-10 22:19:21 +0300

    Update multiplechoicequestion.py
    change description

--- multiplechoicequestion.py
+++ multiplechoicequestion.py
@@ -1,4 +1,4 @@
-'''This program will asked user question with 4 option and accept user answer (a, b, c) and give correct score as a feedbak.
+'''This program will asked user a question with 3 option and accept user answer (a, b, c) and give correct score as a feedback.
     By doing this program, you can understand the how to create and use class, function, list (store answer in list).
     and for loop and if, else statement.
 '''
